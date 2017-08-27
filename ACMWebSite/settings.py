cb983744798bedeedb62a39c0b9bf84f68fe8f7f
Juan Pablo <juanpablorn30@gmail.com>
2017-08-26 21:42:46 -0500

    Change settings for server

--- ACMWebSite/settings.py
+++ ACMWebSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(hyrtj&hq)mv*!h8q*n-_xslu^qt!6i1*yez+)mmw)x=v2-ri$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Adding DigitalOcean
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
