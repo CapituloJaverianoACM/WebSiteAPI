bec5baf6bbb6384c6b7f2b8b6b96f9f24f2e22ec
johan-smc <johan_smc@hotmail.com>
2018-01-29 23:58:53 -0500

    add tutorial api

--- WebSite/urls.py
+++ WebSite/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('members/', views.MemberList.as_view()),
 	path('staff/', views.StaffList.as_view()),
 	path('activities/', views.ActivityList.as_view()),
+	path('tutorials/', views.TutorialList.as_view()),
 	path(
 		'tutorial/<str:id_tutorial>/',
 		views.tutorial_detail,
