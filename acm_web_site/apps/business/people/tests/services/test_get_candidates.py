4435dbebe5d239230f4a0f9096cb604cc7e34885
Camilo Serrano <camiloserrano@javeriana.edu.co>
2020-07-26 14:59:42 -0500

    did everything

---
+++ acm_web_site/apps/business/people/tests/services/test_get_candidates.py
@@ -0,0 +1,21 @@
+import pdb
+
+from django.test import TestCase
+from django.utils import timezone
+
+from mixer.backend.django import mixer
+
+from business.people.models import Member
+from business.people.services import get_candidates
+
+
+class GetCandidatesTest(TestCase):
+
+    def setUp(self):
+        self.teams = mixer.cycle(4).blend(Member, date_chapter=None)
+        self.teams.append(mixer.blend(Member, date_chapter=timezone.now()))
+        self.teams.append(mixer.blend(Member, date_chapter=None))
+
+    def test_should_get_four_candidates(self):
+        response = get_candidates().all()
+        self.assertEquals(response.count(), 5)
---
+++ acm_web_site/apps/business/people/tests/services/test_get_staff.py
@@ -0,0 +1,22 @@
+import pdb
+
+from django.test import TestCase
+
+from mixer.backend.django import mixer
+
+from business.people.models import Team, Member
+from business.people.services import get_teams, get_staff
+
+
+class GetTeamsStaffTest(TestCase):
+    '''def test_all_members_are_staff(self):
+        self.staff = mixer.cycle(3).blend(Member, is_staff=True)
+        response = get_staff().all()
+        self.assertEquals(response.count(), len(self.staff))
+        for team in response:
+            self.assertIn(team, self.staff)'''
+
+    '''def test_no_members_are_staff(self):
+        self.staff = mixer.cycle(3).blend(Member, is_staff=False)
+        response = get_staff().all()
+        self.assertEquals(response.count(), 0))'''
