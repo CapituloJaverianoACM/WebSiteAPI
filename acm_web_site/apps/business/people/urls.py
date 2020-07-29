5a600becc20c2ed4f1f1a3f8ceeee638929caf3e
DorianMoreno <dorian.r.mv@gmail.com>
2020-07-28 22:33:19 -0500

    urls TODO addressed

--- acm_web_site/apps/business/people/urls.py
+++ acm_web_site/apps/business/people/urls.py
@@ -4,8 +4,8 @@ from . import views
 handler404 = 'business.people.views.page_not_found'
 
 urlpatterns = [
-	path('members', views.members),
+	path('members', views.MemberViewSet.as_view(dict(get='list'))),
 	path('teams/', views.TeamList.as_view()),
-	path('joinus/', views.join_us),
+	path('joinus/', views.MemberViewSet.as_view(dict(post='create'))),
 
 ]
