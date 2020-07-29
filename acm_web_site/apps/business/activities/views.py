7c10f994d355565810d69e024cca027329693f27
DorianMoreno <dorian.r.mv@gmail.com>
2020-07-28 22:40:13 -0500

    views.py TODOs addressed

--- acm_web_site/apps/business/activities/views.py
+++ acm_web_site/apps/business/activities/views.py
@@ -56,11 +56,10 @@ class ActivityDetail(APIView):
     def post(self, request, pk):
         member_serializer = MemberSerializer(data=request.data)
         print(member_serializer)
-    # TODO: Handle all info received from form
-    # TODO: Save the many to many relationship in serializer
+    # TODO: Handle all info received from its form instead of printing
+    # TODO: Take into account all many-to-many relationships in the serializer
 
 
-# TODO: find out what this view is for
 class ConfirmActivityView(GenericAPIView):
     serializer_class = ConfirmActivitySerializer
 
@@ -73,7 +72,7 @@ class ConfirmActivityView(GenericAPIView):
         serializer = self.get_serializer(data=self.request.data)
         serializer.is_valid(raise_exception=True)
 
-        # TODO: Add logic to confirm the assistance
+        # TODO: Append new attendee to activity's attendee list
         """
         uid = urlsafe_base64_decode(uidb64)
         user = User.objects.get(pk=uid)
